304276105189d51dd87b65c72036967477df99ec
Ankur Tripathi <ankur@intelligrape.com>
2013-01-29 11:24:48 +0530

    Example for getter and setter

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,19 @@
+import bootcamp.Company
+
 class BootStrap {
 
     def init = { servletContext ->
+        getter()
     }
+
+    void getter() {
+        Company company = new Company(name: "Intelligrape")
+        company.save(flush: true)
+        println "########################################################"
+        println "getter ${company.getName()} property ${company.name}"
+        println "########################################################"
+    }
+
     def destroy = {
     }
 }
